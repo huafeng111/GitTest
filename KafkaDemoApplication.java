041da3ce123f2e607f4a2be147f4d42cd4defd52
huafengmai <510826800@qq.com>
2023-01-31 20:03:15 -0800

    KafkaDemoApplication.java

--- KafkaDemoApplication.java
+++ KafkaDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaDemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) //nb
 		SpringApplication.run(KafkaDemoApplication.class, args);
 	}
 
