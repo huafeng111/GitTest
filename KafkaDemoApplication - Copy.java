8f7fc50c1103492f81b3e7871dc1fcca36e00ddc
huafengmai <510826800@qq.com>
2023-01-31 20:19:44 -0800

    Create KafkaDemoApplication - Copy.java

---
+++ KafkaDemoApplication - Copy.java
@@ -0,0 +1,14 @@
+package com.codedecode.KafkaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoApplication {
+
+	public static void main(String[] args) //nb
+		SpringApplication.run(KafkaDemoApplication.class, args);
+
+	}
+
+}
