1575afb32c20e5fca33ca781b979f3f68fbacd35
huafengmai <510826800@qq.com>
2023-01-31 19:49:49 -0800

    Update KafkaDemoApplication.java

--- KafkaDemoApplication.java
+++ KafkaDemoApplication.java
@@ -1,13 +1,1 @@
-package com.codedecode.KafkaDemo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class KafkaDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaDemoApplication.class, args);
-	}
-
-}
+merge to main
